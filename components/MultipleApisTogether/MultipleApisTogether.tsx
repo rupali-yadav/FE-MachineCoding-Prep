f88e1b9442f386b731db74be443dcfb724ff7af2
rupali-yadav <rupali.yadav0@gmail.com>
2026-03-10 22:19:08 +0530

    multiple apis together practice

--- components/MultipleApisTogether/MultipleApisTogether.tsx
+++ components/MultipleApisTogether/MultipleApisTogether.tsx
@@ -5,25 +5,15 @@ const MultipleApisTogether = () => {
 
     useEffect(() => {
         try {
-            const fetchData = async () => {
-                let apiData1;
-                let apiData2;
 
-                const results: PromiseSettledResult<any>[] = await Promise.allSettled([
+            const fetchData = async () => {
+                const results = await Promise.allSettled([
                     fetch("api-url-1").then((resp) => resp.json()),
                     fetch("api-url-2").then((resp) => resp.json())
                 ]);
-                // Promise.allSettled return an array of responses of these apis
-                // Promise.all() return an array of responses of these 
-                // apis only if all the apis are successful otherwise it will throw an error
 
-                if (results[0]?.status === "fulfilled") {
-                    apiData1 = results[0].value;
-                }
-
-                if (results[1]?.status === "fulfilled") {
-                    apiData2 = results[1].value;
-                }
+                const apiData1 = results[0].status === "fulfilled" ? results[0].value : [];
+                const apiData2 = results[1].status === "fulfilled" ? results[1].value : [];
             }
 
             fetchData();
