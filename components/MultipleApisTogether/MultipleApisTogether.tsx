eb174e43f183a40e83c738e8ef4535bf3e1a2bb0
rupali-yadav <rupali.yadav0@gmail.com>
2026-03-10 22:54:48 +0530

    abort controller

--- components/MultipleApisTogether/MultipleApisTogether.tsx
+++ components/MultipleApisTogether/MultipleApisTogether.tsx
@@ -1,27 +1,38 @@
+import { abort } from "process";
 import { useEffect } from "react"
 
 const MultipleApisTogether = () => {
 
 
     useEffect(() => {
-        try {
+        const controller = new AbortController();
+        const signal = controller.signal;
 
-            const fetchData = async () => {
+        const fetchData = async () => {
+            try {
+
                 const results = await Promise.allSettled([
-                    fetch("api-url-1").then((resp) => resp.json()),
-                    fetch("api-url-2").then((resp) => resp.json())
+                    fetch("api-url-1", { signal }).then((resp) => resp.json()),
+                    fetch("api-url-2", { signal }).then((resp) => resp.json())
                 ]);
 
                 const apiData1 = results[0].status === "fulfilled" ? results[0].value : [];
                 const apiData2 = results[1].status === "fulfilled" ? results[1].value : [];
+
+                // to cancel a api call you can simply call 
+                // abortController.abort();
+
+                // clean up
+                return () => { controller.abort(); }
+                
+            } catch (error) {
+                console.error("Error fetching data from APIs:", error);
             }
 
             fetchData();
 
-        } catch (error) {
-            console.error("Error fetching data from APIs:", error);
         }
-    });
+    }, []);
 
     return (
         <h1>MultipleApisTogether</h1>
